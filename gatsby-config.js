0e558ef2f7616beff9b9edf600cd904dbe90f5f3
Jacine Luisi <jacine.luisi@gmail.com>
2020-07-24 09:23:36 -0400

    Add @reflexjs/gatsby-theme-post and React Disqus.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     description: "Starter for the Reflex.",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
-  plugins: ["@reflexjs/gatsby-theme-base"],
+  plugins: [
+    "@reflexjs/gatsby-theme-base",
+    "@reflexjs/gatsby-theme-post"
+  ],
 }
